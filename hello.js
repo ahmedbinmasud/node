88764e8b93ed6e1f0e6c587e622b748956bc9c2b
ahmedbinmasud <126449375+ahmedbinmasud@users.noreply.github.com>
2023-08-06 15:22:03 +0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env nodejs
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello mein ahmed houn and this is my node app\n');
+}).listen(8091, 'localhost');
+console.log('Server running at http://localhost:8091/');
+
