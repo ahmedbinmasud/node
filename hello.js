ee4c9dfee352198bac377e81da93677bb1b18b5f
ahmedbinmasud <126449375+ahmedbinmasud@users.noreply.github.com>
2023-08-18 07:33:15 +0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello mein ahmed houn and this is my node app\n');
-}).listen(8091, 'localhost');
-console.log('Server running at http://localhost:8091/');
+}).listen(3000, 'localhost');
+console.log('Server running at http://localhost:3000/');
 
