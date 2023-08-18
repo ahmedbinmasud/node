83327949bbe5a70b52223e5ba0754017be93737d
ahmedbinmasud <126449375+ahmedbinmasud@users.noreply.github.com>
2023-08-18 10:46:25 +0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,12 @@
-#!/usr/bin/env nodejs
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello mein ahmed houn and this is my node app\n');
-}).listen(3000, 'localhost');
-console.log('Server running at http://localhost:3000/');
+const http = require('http');
 
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, Jenkins Node.js App!\n');
+});
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
